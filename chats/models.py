8fc5ab1a53bbe3751a032e1e0e3318cf2643b0bc
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-28 15:27:28 +0300

    add: chat model

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import CustomUser
 
+
+class Chat(models.Model):
+    chat_id = models.CharField(primary_key=True)
+    chat_message = models.CharField(max_length=300, blank=True)
+    chat_merchant_user_id = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE)
+    chat_farmer_user_id = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE)
 # Create your models here.
