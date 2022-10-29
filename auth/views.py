4e7ea1238e3497c89b6b363abcdbeaf9bd8c84f7
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-29 04:08:41 +0300

    update: add token obtain view and sign up view

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from auth.serializers import MyTokenObtainPairSerializer, RegisterSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    # for simplejwt authentication
+    serializer_class = MyTokenObtainPairSerializer
+
+
+def signUp(request):
+    pass
