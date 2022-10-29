3e69e6d704ec35bdc5409f26c4e5ecce7af5a641
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-29 04:07:44 +0300

    add: simplejwt serializers

---
+++ auth/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from users.models import CustomUser
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+# for simplejwt authentication
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['name'] = user.username
+
+        return token
