41c8e432b199e8f7a8b12b1704ba298dd4ef8704
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-29 04:13:28 +0300

    add: CustomUserAdmin for the admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from .models import CustomUser, Farmer, Merchant
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+    list_display = ["email", "username", "user_role"]
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'User role',
+            {
+                'fields': (
+                    'user_role',
+                )
+            }
+        )
+    )
+
 
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Merchant)
+admin.site.register(Farmer)
