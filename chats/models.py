5270b823a74c1c2108fb089a1d1381a8f87203cb
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-29 04:10:33 +0300

    update: Cahnge to repective tables

--- chats/models.py
+++ chats/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from users.models import CustomUser
+from users.models import CustomUser, Farmer, Merchant
 
 
 class Chat(models.Model):
-    chat_id = models.CharField(primary_key=True)
+    chat_id = models.AutoField(primary_key=True)
     chat_message = models.CharField(max_length=300, blank=True)
-    chat_merchant_user_id = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE)
-    chat_farmer_user_id = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE)
+    chat_user_merchant_id = models.ForeignKey(
+        Merchant, on_delete=models.CASCADE)
+    chat_user_farmer_id = models.ForeignKey(
+        Farmer, on_delete=models.CASCADE)
 # Create your models here.
