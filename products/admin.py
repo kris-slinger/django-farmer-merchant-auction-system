382ffd07fbbdaa4c60fbd18f9beab9ac534c21cb
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-29 04:10:17 +0300

    register: models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
+from itertools import product
 from django.contrib import admin
 
+from .models import Product, ProductCategory, ProductFile
+
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductFile)
 # Register your models here.
