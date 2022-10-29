ec9592e638c76b1a507e982eec325dfa49d805bc
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-29 04:13:59 +0300

    add: users

--- users/models.py
+++ users/models.py
@@ -1,12 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from choices.choices import USER_ROLES
 
 
-class CustomUser():
-    user_name = models.CharField(verbose_name="username", max_length=100)
-    user_national_id = models.IntegerField(max_length=10_000_000)
-    user_phone = models.CharField(max_length=100)
-    user_email = models.EmailField(verbose_name="username")
-    user_password = models.CharField(verbose_name="password")
-    user_role = models.CharField(max_length=2, choices=USER_ROLES)
-# Create your models here.
+'''
+Django abstract user contains username,email and password fields. 
+This is why they have not been included in the Custom user model.
+'''
+
+
+class CustomUser(AbstractUser):
+    user_id = models.AutoField(primary_key=True)
+
+    user_national_id = models.IntegerField(default=0)
+    user_phone = models.CharField(max_length=100, default=" ")
+    user_role = models.CharField(
+        max_length=2, choices=USER_ROLES, blank=True)
+
+
+class Farmer(models.Model):
+    farmer_id = models.AutoField(primary_key=True)
+    user_farmer_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_farmer_id.username
+
+
+class Merchant(models.Model):
+    farmer_id = models.AutoField(primary_key=True)
+    user_merchant_id = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_merchant_id.username
