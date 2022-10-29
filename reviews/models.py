dea0e7e05381718b7a7c554d25e088b1975d65cd
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-29 04:15:52 +0300

    update: review fields

--- reviews/models.py
+++ reviews/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-from ..users.models import CustomUser
+from users.models import CustomUser
+from users.models import Farmer, Merchant
 
 
 class Review(models.Model):
-    review_id = models.CharField(primary_key=True)
-    review_rating = models.IntegerField(choices=[0, 1, 2, 3, 4, 5])
+    review_id = models.AutoField(primary_key=True)
+    review_rating = models.IntegerField(
+        choices=[(0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])
     review_message = models.CharField(max_length=300, blank=True)
-    review_merchant_user_id = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE)
-    review_farmer_user_id = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE)
+    review_receiver = models.ForeignKey(
+        Merchant, on_delete=models.CASCADE)
+    review_sender = models.ForeignKey(
+        Farmer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.review_receiver} <- {self.review_sender}"
+
 # Create your models here.
