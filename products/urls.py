943e3df4f8374c1dd0abf7d3d4b537b2e5c0e0fb
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 00:07:48 +0300

    add: url patterns

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from products.models import ProductCategory
 
-from products.views import ProductFileView, ProductView
+from products.views import ProductDetailView, ProductFileDetailView, ProductFileView, ProductView
 
 urlpatterns = [
     path('', ProductView.as_view()),
+    path('<int:productId>', ProductDetailView.as_view()),
+    path('productFile/<int:productFileId>', ProductFileDetailView.as_view()),
 ]
