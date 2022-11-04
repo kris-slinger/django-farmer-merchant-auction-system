e18dbf47e3d33e36655d83d8bc8d3456201dd60a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 00:04:38 +0300

    add: orderView

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from orders.models import Order
 
-# Create your views here.
+from orders.serializer import OrderSerialier
+
+
+class OrderView(APIView):
+    def get(self, request):
+        queryset = Order.objects.all()
+        serializer = OrderSerialier(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = OrderSerialier(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
