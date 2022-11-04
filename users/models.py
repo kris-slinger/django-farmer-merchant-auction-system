d1751f59af89136b851b7427ace7745ef3e97503
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-04 10:12:38 +0300

    add: customUser and customUsermanager

--- users/models.py
+++ users/models.py
@@ -1,36 +1,78 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from choices.choices import USER_ROLES
 
 
-'''
-Django abstract user contains username,email and password fields. 
-This is why they have not been included in the Custom user model.
-'''
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password, **other_fields):
+        if not email:
+            raise ValueError("You must provide an email")
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email, username=username, **other_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, username, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+        if other_fields.get('is_superuser') is not True:
+            return ValueError(
+                'Superuser must be assigned to is_staff=True'
+            )
+
+        if other_fields.get('is_superuser') is not True:
+            return ValueError(
+                'Superuser must be assigned to is_superuser=True'
+            )
+
+        return self.create_user(email, username, password, **other_fields)
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    list_filter = 'user_role'
     user_id = models.AutoField(primary_key=True)
-    user_name=models.CharField(max_length=200)
-    user_password=models.CharField(max_length=200)
+    email = models.EmailField(unique=True, db_column="user_email")
+    username = models.CharField(
+        max_length=200, unique=True, db_column="user_name")
+    password = models.CharField(max_length=200, db_column="user_password")
     user_national_id = models.IntegerField(default=0)
-    user_phone = models.CharField(max_length=100, default=" ")
+    user_phone = models.CharField(max_length=100, default="")
     user_role = models.CharField(
         max_length=2, choices=USER_ROLES, blank=True)
+    is_superuser = models.BooleanField(
+        default=False, db_column="user_is_superuser")
+    is_staff = models.BooleanField(
+        default=False, db_column="user_is_staff")
+    is_active = models.BooleanField(
+        default=True, db_column="user_is_active")
+    last_login = models.DateTimeField(
+        blank=True, null=True, db_column="user_last_login")
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
 
 
 class Farmer(models.Model):
     farmer_id = models.AutoField(primary_key=True)
-    user_farmer_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    farmer_user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user_farmer_id.username
+        return self.farmer_user_id.username
 
 
 class Merchant(models.Model):
     farmer_id = models.AutoField(primary_key=True)
-    user_merchant_id = models.OneToOneField(
+    merchant_user_id = models.OneToOneField(
         CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user_merchant_id.username
+        return self.merchant_user_id.username
