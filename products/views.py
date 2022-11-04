e4bc3fcc42e045eb90a3edd05c3c937a3f2217ac
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 00:07:25 +0300

    add:

--- products/views.py
+++ products/views.py
@@ -5,8 +5,10 @@ from products.serializer import ProductCategorySerializer, ProductFileSerializer
 
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.http import Http404
 # TODO: Figure out who will have permission for this view
+
+
 class ProductCategory(APIView):
     def get(self, request):
         queryset = ProductCategory.objects.all()
@@ -30,10 +32,35 @@ class ProductFileView(APIView):
     def post(self, request):
         serializer = ProductFileSerializer(data=request.data)
         if serializer.valid():
-            
+
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductFileDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            ProductFile.objects.get(pk)
+        except ProductFile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, productFileId):
+        query = self.get_object(productFileId)
+        serializer = ProductFileSerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, productFileId):
+        query = self.get_object(productFileId)
+        serializer = ProductFileSerializer(query, data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, productFileId):
+        query = self.get_object(productFileId)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class ProductView(APIView):
     def get(self, request):
@@ -48,3 +75,27 @@ class ProductView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ProductDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(product_id=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, productId):
+        query = self.get_object(productId)
+        print(query)
+        serializer = ProductSerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, productId):
+        query = self.get_object(productId)
+        serializer = ProductSerializer(query, data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, productId):
+        query = self.get_object(productId)
+        return Response(status=status.HTTP_204_NO_CONTENT)
