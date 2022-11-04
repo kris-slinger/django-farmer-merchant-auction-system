de6b9231623b6a72e6060f3dbff3631e7e1b2f17
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-04 10:15:23 +0300

    add: user related views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.models import Farmer, Merchant
+from users.serializers import CustomUserSerializer, FarmerSerializer, MerchantSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class CustomUserView(APIView):
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            customUser = serializer.save()
+            if serializer.validated_data['user_role'] == 'Fm':
+                Farmer(farmer_user_id=customUser).save()
+            if serializer.validated_data['user_role'] == 'Mt':
+                Merchant(merchant_user_id=customUser).save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FarmerView(APIView):
+    def get(self, request):
+        queryset = Farmer.objects.all()
+        serializer = FarmerSerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class MerchantView(APIView):
+    def get(self, request):
+        queryset = Merchant.objects.all()
+        serializer = MerchantSerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
