097597d7a91738acd323549b4d9bef53369db16c
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 00:08:29 +0300

    update: field

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,4 @@
+from random import choices
 from django.db import models
 
 from users.models import CustomUser
@@ -6,9 +7,10 @@ from users.models import Farmer, Merchant
 
 class Review(models.Model):
     review_id = models.AutoField(primary_key=True)
-    review_rating = models.IntegerField(
-        choices=[(0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])
     review_message = models.CharField(max_length=300, blank=True)
+    review_rating = models.IntegerField(
+        choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')]
+    )
     review_merchant_id = models.ForeignKey(
         Merchant, on_delete=models.CASCADE)
     review_farmer_id = models.ForeignKey(
@@ -16,4 +18,3 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.review_farmer_id} <-- {self.review_merchant_id}"
-
