aef99b075bb0e9f77b5f68d930df5646b0e1857c
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-05 00:11:21 +0300

    update: file

--- users/views.py
+++ users/views.py
@@ -1,12 +1,17 @@
 from rest_framework.views import APIView
 
-from users.models import Farmer, Merchant
+from users.models import CustomUser, Farmer, Merchant
 from users.serializers import CustomUserSerializer, FarmerSerializer, MerchantSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
 
 class CustomUserView(APIView):
+    def get(self, request):
+        queryset = CustomUser.objects.all()
+        serializer = CustomUserSerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
     def post(self, request):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
@@ -18,16 +23,16 @@ class CustomUserView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class FarmerView(APIView):
-    def get(self, request):
-        queryset = Farmer.objects.all()
-        serializer = FarmerSerializer(queryset, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-
-
-class MerchantView(APIView):
-    def get(self, request):
-        queryset = Merchant.objects.all()
-        serializer = MerchantSerializer(queryset, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
+# TODO: will be done via admin page
+# class FarmerView(APIView):
+#     def get(self, request):
+#         queryset = Farmer.objects.all()
+#         serializer = FarmerSerializer(queryset, many=True)
+#         return Response(serializer.data, status.HTTP_200_OK)
+#
+#
+# class MerchantView(APIView):
+#     def get(self, request):
+#         queryset = Merchant.objects.all()
+#         serializer = MerchantSerializer(queryset, many=True)
+#         return Response(serializer.data, status.HTTP_200_OK)
