1539b431acf72ece7ec7944f73d197297fc8cb9d
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-04 10:11:43 +0300

    add: paths

--- server/urls.py
+++ server/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from webbrowser import get
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("auth.urls"))
+    path('auth/', include("auth.urls")),
+    path('users/', include("users.urls")),
+    path('docs/', include_docs_urls(title="Farmer merchant auction system")),
+    path('products/', include("products.urls")),
+    path('schema/', get_schema_view(
+        title='Farmer merchant auction system',
+        description='API for all things',
+        version='1.0.0'
+    ), name="openapi-schema")
 ]
