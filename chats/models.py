c9c5dc96d98c561b79458f078215f6db1b7830af
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-04 10:05:37 +0300

    update: change model field

--- chats/models.py
+++ chats/models.py
@@ -5,11 +5,11 @@ from users.models import CustomUser, Farmer, Merchant
 class Chat(models.Model):
     chat_id = models.AutoField(primary_key=True)
     chat_message = models.CharField(max_length=300, blank=True)
-    chat_user_merchant_id = models.ForeignKey(
+    chat_merchant_id = models.ForeignKey(
         Merchant, on_delete=models.CASCADE)
-    chat_user_farmer_id = models.ForeignKey(
+    chat_farmer_id = models.ForeignKey(
         Farmer, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.chat_user_farmer_id} - {self.chat_user_merchant_id}"
+        return f"{self.chat_farmer_id} - {self.chat_merchant_id}"
 # Create your models here.
