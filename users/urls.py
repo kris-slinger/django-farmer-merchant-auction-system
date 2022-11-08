c182625809b73c125780b9d4e1a0ebe222599899
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-09 00:48:11 +0300

    add: user url patterns

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CustomUserView, FarmerView, MerchantView
 
 urlpatterns = [
-    # path('', CustomUserView.as_view()),
-    # path('farmer/', FarmerView.as_view()),
-    # path('merchant/', MerchantView.as_view())
+    path('', CustomUserView.as_view()),
+    path('farmer/', FarmerView.as_view()),
+    path('merchant/', MerchantView.as_view())
 ]
