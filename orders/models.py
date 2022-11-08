1917989ef1659fb6541050b0922793aa60650719
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-09 00:38:20 +0300

    add: order_price field and default values to some order fields

--- orders/models.py
+++ orders/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 
 from choices.choices import ORDER_STATUS_CHOICES
-from users.models import Farmer
+from users.models import Merchant
 from products.models import Product
-
 
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
     order_name = models.CharField(max_length=100)
-    order_quantity = models.IntegerField()
-    order_status = models.CharField(
-        max_length=10, choices=ORDER_STATUS_CHOICES)
-    order_total_price = models.DecimalField(
+    order_quantity = models.IntegerField(default=1)
+    order_price = models.DecimalField(
         max_digits=8, decimal_places=2, null=True)
+    order_status = models.CharField(
+        max_length=10, choices=ORDER_STATUS_CHOICES,default="pending")
+    order_total_price = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True,blank=True)
     order_creation_date = models.DateField(auto_now_add=True)
     order_expiration_date = models.DateField(blank=True, null=True)
     order_product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     order_merchant_id = models.ForeignKey(
-        Farmer, on_delete=models.CASCADE)
+        Merchant, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.order_name
