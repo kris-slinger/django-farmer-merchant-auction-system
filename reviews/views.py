6aff246779c086494f1c95189551084c0b290f04
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-09 00:44:38 +0300

    add: merchant_instance and delete functionality

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,18 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+from permissions.permissions import IsMerchantOrReadOnly
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
 from django.http import Http404
 
+from users.models import Merchant
+
 
 class ReviewView(APIView):
+    permission_classes = [IsAuthenticated, IsMerchantOrReadOnly]
     def get(self, request):
         queryset = Review.objects.all()
         serializer = ReviewSerializer(queryset, many=True)
@@ -16,7 +22,8 @@ class ReviewView(APIView):
         serializer = ReviewSerializer(
             data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            merchant_instance=Merchant.objects.get(merchant_user_id=request.user)
+            serializer.save(review_merchant_id=merchant_instance)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -43,5 +50,5 @@ class ReviewDetailView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, reviewId):
-        query = self.get_object(reviewId)
+        query = self.get_object(reviewId).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
