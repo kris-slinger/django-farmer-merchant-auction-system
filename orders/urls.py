de012b6cff9488a8c38ca9aa9b96aba31e62ad4a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-09 00:48:38 +0300

    add: order urlpatterns

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from orders.views import OrderView
+from orders.views import OrderView, OrderDetailView
 urlpatterns = [
-    path('', OrderView.as_view())
+    path('', OrderView.as_view()),
+    path('<int:orderId>', OrderDetailView.as_view())
 ]
