cae9e6bf9c49fa7636dbb03bf6318da6ca3b31fa
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-09 00:43:41 +0300

    update: override some serializer model fields to become readonly

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from reviews.models import Review
+from users.serializers import FarmerSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    receiver=serializers.ReadOnlyField(source="review_farmer_id.farmer_user_id.username")
+    sender=serializers.ReadOnlyField(source="review_merchant_id.merchant_user_id.username")
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = ['review_id','review_message','review_rating','sender','receiver','review_farmer_id']
