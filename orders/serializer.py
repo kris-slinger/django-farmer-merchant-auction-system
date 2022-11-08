29348202e12cba416e1c18f1758dc4973d96379f
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-09 00:48:59 +0300

    update: overide orderSerializer model fields to become readonly

--- orders/serializer.py
+++ orders/serializer.py
@@ -4,9 +4,16 @@ from products.serializer import ProductSerializer
 from users.serializers import MerchantSerializer
 
 
+# TODO: calculate order_total_price,order_expiration_date using a signal
+
 class OrderSerializer(serializers.ModelSerializer):
     # order_product_id = ProductSerializer()
-    # order_merchant_id = MerchantSerializer()
+    order_merchant_name = serializers.ReadOnlyField(source="order_merchant_id.merchant_user_id.username")
+    order_total_price = serializers.ReadOnlyField()
+    order_expiration_date = serializers.ReadOnlyField()
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['order_id', 'order_name', 'order_quantity','order_price','order_status', 'order_total_price', 'order_total_price',
+                  'order_creation_date',
+                  'order_expiration_date', 'order_merchant_name']
--- orders/views.py
+++ orders/views.py
@@ -1,21 +1,48 @@
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from orders.models import Order
-
-from orders.serializer import OrderSerialier
-
-
+from orders.models import Merchant
+from orders.serializer import OrderSerializer
+from permissions.permissions import IsMerchantOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 class OrderView(APIView):
+    permission_classes = [IsAuthenticated, IsMerchantOrReadOnly]
     def get(self, request):
         queryset = Order.objects.all()
-        serializer = OrderSerialier(queryset, many=True)
+        serializer = OrderSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = OrderSerialier(data=request.data)
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            merchant_instance = Merchant.objects.get(merchant_user_id=request.user)
+            serializer.save(order_merchant_id=merchant_instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class OrderDetailView(APIView):
+    permission_classes = [IsAuthenticated, IsMerchantOrReadOnly]
+    def get_object(self, pk):
+        try:
+            return Order.objects.get(order_id=pk)
+        except Order.DoesNotExist:
+            raise Http404
+    def get(self,request,orderId):
+        query = self.get_object(orderId)
+        serializer = OrderSerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self,request,orderId):
+        query=self.get_object(orderId)
+        serializer=OrderSerializer(query,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,orderId):
+        query=self.get_object(orderId).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
