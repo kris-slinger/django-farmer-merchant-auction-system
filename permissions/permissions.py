d2b43d1328671372664abadf8dad1ba97999469e
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-09 22:01:22 +0300

    update: file

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -1,15 +1,17 @@
 from rest_framework import permissions
 
+
 class IsMerchantOrReadOnly(permissions.BasePermission):
-    message="operation not allowed"
+    message = "operation not allowed"
+
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS or request.user.user_role =='merchant' or (request.user.is_superuser and request.method!='POST') or (request.user.user_role=='farmer' and request.method=='PUT'):
+        if request.method in permissions.SAFE_METHODS or request.user.user_role == 'merchant' or (request.user.is_superuser and request.method != 'POST') or (request.user.user_role == 'farmer' and request.method == 'PUT'):
             return True
         return False
+
 
 class IsFarmerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS or request.user.user_role =='farmer' or request.user.is_superuser:
+        if request.method in permissions.SAFE_METHODS or request.user.user_role == 'farmer' or request.user.is_superuser:
             return True
         return False
-
