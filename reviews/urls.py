2128f347593f8be37e812cac74101a0180455196
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-16 04:59:08 +0300

    add: url review and rating patterns

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from reviews.views import ReviewDetailView, ReviewView
+from reviews.views import ReviewDetailView, ReviewRating, ReviewView
 urlpatterns = [
     path('', ReviewView.as_view()),
-    path('<int:reviewId>/', ReviewDetailView.as_view())
+    path('<int:reviewId>/', ReviewDetailView.as_view()),
+    path('rating/', ReviewRating.as_view())
 
 ]
