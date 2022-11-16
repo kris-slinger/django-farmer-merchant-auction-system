05ae1d9a5e88b30bab665347d95bc80a0d67b69c
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-16 04:57:01 +0300

    add: product serializer update function

--- products/serializer.py
+++ products/serializer.py
@@ -2,6 +2,7 @@ from itertools import product
 from rest_framework import serializers
 from products.models import ProductCategory, Product, ProductFile
 from users.serializers import FarmerSerializer
+from drf_extra_fields.fields import Base64ImageField
 
 
 class ProductCategorySerializer(serializers.ModelSerializer):
@@ -12,6 +13,8 @@ class ProductCategorySerializer(serializers.ModelSerializer):
 
 
 class ProductFileSerializer(serializers.ModelSerializer):
+    product_file_image = Base64ImageField()
+
     class Meta:
         model = ProductFile
         fields = ['product_file_id', 'product_file_name', 'product_file_image']
@@ -24,46 +27,42 @@ class ProductSerializer(serializers.ModelSerializer):
         source="product_product_file_id")
     product_farmer_name = serializers.ReadOnlyField(
         source="product_farmer_id.farmer_user_id.username")
-    product_category_name = serializers.ReadOnlyField(
-        source="product_category_id.product_category_name")
+    product_farmer_phone = serializers.ReadOnlyField(
+        source="product_farmer_id.farmer_user_id.user_phone"
+    )
 
     class Meta:
         model = Product
-        fields = ['product_id', 'product_name', 'product_price', 'product_location', 'product_description',
-                  'product_category_name', 'product_farmer_name', 'product_category_data', 'product_product_file_data']
+        fields = ['product_id', 'product_name', 'product_price', 'product_farmer_phone', 'product_location', 'product_description', 'product_created_at',
+                  'product_farmer_name', 'product_category_data', 'product_product_file_data']
 
     def create(self, validated_data):
-        print(validated_data)
-        product_file_data = dict(
-            validated_data.pop('product_product_file_id'))
-        product_category_data = dict(
-            validated_data.pop('product_category_id'))
         product_category_instance = ProductCategory.objects.create(
-            **product_category_data)
-        product_file_instance = ProductFile.objects.create(**product_file_data)
-        print(product_file_instance)
+            **dict(validated_data.pop('product_category_id')))
+        product_file_instance = ProductFile.objects.create(**dict(
+            validated_data.pop('product_product_file_id')))
         product = Product.objects.create(**validated_data, product_category_id=product_category_instance,
                                          product_product_file_id=product_file_instance)
         return product
 
+    def update(self, instance, validated_data):
+        ProductCategory.objects.filter(product_category_id=instance.product_category_id.product_category_id).update(
+            **dict(validated_data.pop("product_category_id"))
+        )
+        file_instance = ProductFile.objects.create(
+            **dict(validated_data.pop('product_product_file_id'))
+        )
+        ProductFile.objects.get(
+            product_file_id=instance.product_product_file_id.product_file_id).delete()
 
-# class ProductMainSerializer(serializers.Serializer):
-#     product_category_id = ProductCategorySerializer()
-#     product_product_file_id = ProductFileSerializer()
-#     product_name = serializers.CharField(max_length=200)
-#     product_price = serializers.DecimalField(max_digits=20, decimal_places=2)
-#     product_location = serializers.CharField(max_length=200)
-#     product_description = serializers.CharField()
-# 
-#     def create(self, validated_data):
-#         product_file_data = dict(
-#             validated_data.pop('product_product_file_id'))
-#         product_category_data = dict(
-#             validated_data.pop('product_category_id'))
-#         product_category_instance = ProductCategory.objects.create(
-#             **product_category_data)
-#         product_file_instance = ProductFile.objects.create(**product_file_data)
-#         print(product_file_instance)
-#         product = Product.objects.create(**validated_data, product_category_id=product_category_instance,
-#                                          product_product_file_id=product_file_instance)
-#         return product
+        instance.product_product_file_id = file_instance
+        instance.product_name = validated_data.get(
+            'product_name', instance.product_name)
+        instance.product_price = validated_data.get(
+            'product_price', instance.product_price)
+        instance.product_location = validated_data.get(
+            'product_location', instance.product_location)
+        instance.product_description = validated_data.get(
+            'product_description', instance.product_description)
+        instance.save()
+        return instance
