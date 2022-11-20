df428f0307e0f7246ce6aadaba19d4cfd0a8d6b9
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-20 23:36:38 +0300

    update: add urlpattern paths

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from orders.views import OrderView, OrderDetailView
+from orders.views import MerchantSenderOrderView, OrderFarmerUpdateStatusView, OrderView, OrderDetailView, FarmerReceiverOrderView
 urlpatterns = [
     path('', OrderView.as_view()),
-    path('<int:orderId>', OrderDetailView.as_view())
+    path('merchant/', MerchantSenderOrderView.as_view()),
+    path('farmer/', FarmerReceiverOrderView.as_view()),
+    path('<int:orderId>', OrderDetailView.as_view()),
+    path('<int:orderId>/farmer/update', OrderFarmerUpdateStatusView.as_view())
 ]
