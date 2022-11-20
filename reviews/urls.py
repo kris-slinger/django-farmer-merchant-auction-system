b8836e4204a6b57860c330efeb89346bb3691fad
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-20 23:39:54 +0300

    add: url path

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from reviews.views import ReviewDetailView, ReviewRating, ReviewView
+from reviews.views import ReceivedFarmerReviewsView, ReviewDetailView, ReviewRating, ReviewView
 urlpatterns = [
     path('', ReviewView.as_view()),
+    path('farmer/', ReceivedFarmerReviewsView.as_view()),
     path('<int:reviewId>/', ReviewDetailView.as_view()),
     path('rating/', ReviewRating.as_view())
 
