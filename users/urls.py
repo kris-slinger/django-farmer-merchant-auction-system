320fb92498503496d38b3d2a9c8e8f422cd0232a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-20 23:40:58 +0300

    add: path

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomUserView, FarmerView, MerchantView
+from .views import CustomUserView, FarmerView, MerchantView, UserRoleView
 
 urlpatterns = [
     path('', CustomUserView.as_view()),
     path('farmer/', FarmerView.as_view()),
-    path('merchant/', MerchantView.as_view())
+    path('merchant/', MerchantView.as_view()),
+    path('role/', UserRoleView.as_view())
 ]
