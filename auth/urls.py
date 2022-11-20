2036698823ea1acc71009a3936dd57febec80ae2
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-20 23:34:12 +0300

    update: remove path from urlpatterns

--- auth/urls.py
+++ auth/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from auth.views import MyTokenObtainPairView, signUp
+from auth.views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signup/', signUp, name="sign-up")
 ]
